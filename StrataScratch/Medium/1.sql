da9463c792a97bb4691c62e19855f5293e3acef9
Abhinav_Tiwari <106558066+Abhinav-Tiwarii@users.noreply.github.com>
2024-12-07 19:04:55 +0530

    Update 1.sql

--- StrataScratch/Medium/1.sql
+++ StrataScratch/Medium/1.sql
@@ -1,5 +1,5 @@
 --Ranking Most Active Guests
-
+/*
 Rank guests based on the total number of messages they've exchanged with any of the hosts. 
 Guests with the same number of messages as other guests should have the same rank. 
 Do not skip rankings if the preceding rankings are identical.
@@ -19,7 +19,7 @@ ts_accepted_at:timestamp
 ts_booking_at:timestamp
 ts_contact_at:timestamp
 ts_reply_at:timestamp
-
+*/
 ---Sql Query--
 select id_guest, 
 dense_rank() over(order by sum(n_messages) desc) as ranks, 
